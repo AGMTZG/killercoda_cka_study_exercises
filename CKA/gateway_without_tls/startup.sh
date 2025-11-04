8559beb9d3890c7dc236cfb82924f41ad29a3acb
agmtzg <5817993+AGMTZG@users.noreply.github.com>
2025-11-04 11:58:28 -0600

    We updated the startup.sh

--- CKA/gateway_without_tls/startup.sh
+++ CKA/gateway_without_tls/startup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
+echo "[INFO] Installing MetalLB..."
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
+echo "[INFO]  Waiting for MetalLB pods..."
+kubectl wait --namespace metallb-system --for=condition=ready pod --all --timeout=120s || true
 IP=$(ip -4 addr show enp1s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n1)
 PREFIX=$(echo $IP | cut -d'.' -f1-3)
 RANGE_START="${PREFIX}.240"
@@ -20,11 +23,15 @@ metadata:
   name: advert
   namespace: metallb-system
 EOF
+echo "[INFO] Installing Gateway..."
 kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v2.1.0" | kubectl apply -f -
 kubectl apply --server-side -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v2.1.0/deploy/crds.yaml
 kubectl apply -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v2.1.0/deploy/default/deploy.yaml
+echo "[INFO] Installing NGINX Ingress Controller..."
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/cloud/deploy.yaml
+echo "[INFO] Waiting for ingress-nginx pods..."
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --all --timeout=180s || true
+echo "[INFO] Installing setup..."
 cat <<'EOF' | kubectl apply -f -
 apiVersion: apps/v1
 kind: Deployment
