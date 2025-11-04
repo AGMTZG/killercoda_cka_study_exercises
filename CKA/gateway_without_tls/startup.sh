25bc5faa244d362a90ccfaebd29b0b703561a6ff
agmtzg <5817993+AGMTZG@users.noreply.github.com>
2025-11-04 12:31:56 -0600

    Testing final version of startup.sh

--- CKA/gateway_without_tls/startup.sh
+++ CKA/gateway_without_tls/startup.sh
@@ -7,7 +7,7 @@ IP=$(ip -4 addr show enp1s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n1)
 PREFIX=$(echo $IP | cut -d'.' -f1-3)
 RANGE_START="${PREFIX}.240"
 RANGE_END="${PREFIX}.250"
-envsubst < /root/setup/metallb.yaml | kubectl apply -f -
+envsubst < /root/metallb.yaml | kubectl apply -f -
 echo "[INFO] Installing Gateway..."
 kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v2.1.0" | kubectl apply -f -
 kubectl apply --server-side -f https://raw.githubusercontent.com/nginx/nginx-gateway-fabric/v2.1.0/deploy/crds.yaml
@@ -17,5 +17,5 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main
 echo "[INFO] Waiting for ingress-nginx pods..."
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --all --timeout=180s || true
 echo "[INFO] Installing setup..."
-envsubst < /root/setup/setup.yaml | kubectl apply -f -
+envsubst < /root/setup.yaml | kubectl apply -f -
 echo "[INFO] Environment ready"
