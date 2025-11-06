62ee4133843010103911657958d2c69b0102d9eb
agmtzg <5817993+AGMTZG@users.noreply.github.com>
2025-11-06 13:11:37 -0600

    Refined scenario

--- CKA/updating-cluster/startup.sh
+++ CKA/updating-cluster/startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 swapoff -a
 modprobe -a br_netfilter overlay
@@ -7,7 +8,7 @@ sysctl -w net.ipv6.conf.all.forwarding=1
 sysctl -w net.bridge.bridge-nf-call-iptables=1
 sysctl -w net.bridge.bridge-nf-call-ip6tables=1
 apt-get update
-apt-get install -y apt-transport-https ca-certificates curl gpg
+apt-get install -y apt-transport-https ca-certificates curl gpg > /dev/null
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
