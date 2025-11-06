83a423ddf429b29987387b8badc7733e645ef0e9
agmtzg <5817993+AGMTZG@users.noreply.github.com>
2025-11-06 15:20:13 -0600

    Testing a new command

--- CKA/updating-cluster/startup.sh
+++ CKA/updating-cluster/startup.sh
@@ -11,6 +11,7 @@ apt-get update
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
+export DEBIAN_FRONTEND=noninteractive
 apt-get install -y kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 systemctl enable --now kubelet
