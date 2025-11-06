2926991ec200ddb44201911351c920d407cd59a8
agmtzg <5817993+AGMTZG@users.noreply.github.com>
2025-11-06 15:49:03 -0600

    Testing startup.sh

--- CKA/updating-cluster/startup.sh
+++ CKA/updating-cluster/startup.sh
@@ -12,7 +12,16 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.33/deb/Release.key | gpg --dearm
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.33/deb/ /' | tee /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -y kubelet kubeadm kubectl
+apt-get install -y --no-install-recommends \
+    -o Dpkg::Options::="--force-confdef" \
+    -o Dpkg::Options::="--force-confold" \
+    kubelet kubeadm kubectl
 apt-mark hold kubelet kubeadm kubectl
 systemctl enable --now kubelet
+dpkg --configure -a
+mandb -c || true
+if command -v needrestart >/dev/null; then
+    NEEDRESTART_MODE=l needrestart || true
+fi
+systemctl daemon-reload
 kubeadm init --cri-socket=unix:///var/run/containerd/containerd.sock
