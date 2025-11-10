6206720aa8388cef5fec92ee02bb61a2ea6b9894
agmtzg <5817993+AGMTZG@users.noreply.github.com>
2025-11-10 13:09:11 -0600

    Test 3

--- CKA/renew-certificates/startup.sh
+++ CKA/renew-certificates/startup.sh
@@ -9,7 +9,22 @@ mkdir -p "${ETCD_DIR}"
 openssl genrsa -out ${PKI_DIR}/ca.key 2048
 openssl req -x509 -new -nodes -key ${PKI_DIR}/ca.key -subj "/CN=kubernetes-ca" -days 1095 -out ${PKI_DIR}/ca.crt
 
-for cert in front-proxy-client apiserver-kubelet-client apiserver-etcd-client etcd/healthcheck-client etcd/peer; do
+openssl genrsa -out ${PKI_DIR}/front-proxy-ca.key 2048
+openssl req -x509 -new -nodes -key ${PKI_DIR}/front-proxy-ca.key -subj "/CN=front-proxy-ca" -days 1095 -out ${PKI_DIR}/front-proxy-ca.crt
+
+openssl genrsa -out ${ETCD_DIR}/ca.key 2048
+openssl req -x509 -new -nodes -key ${ETCD_DIR}/ca.key -subj "/CN=etcd-ca" -days 1095 -out ${ETCD_DIR}/ca.crt
+
+openssl genrsa -out ${PKI_DIR}/sa.key 2048
+openssl rsa -in ${PKI_DIR}/sa.key -pubout -out ${PKI_DIR}/sa.pub
+
+NORMAL_CERTS=(
+  "apiserver-etcd-client"
+  "apiserver-kubelet-client"
+  "front-proxy-client"
+)
+
+for cert in "${NORMAL_CERTS[@]}"; do
   dir=$(dirname "$cert")
   [ "$dir" != "." ] && mkdir -p "$PKI_DIR/$dir"
   openssl genrsa -out ${PKI_DIR}/${cert}.key 2048
@@ -18,6 +33,23 @@ for cert in front-proxy-client apiserver-kubelet-client apiserver-etcd-client et
     -CA ${PKI_DIR}/ca.crt -CAkey ${PKI_DIR}/ca.key -CAcreateserial \
     -out ${PKI_DIR}/${cert}.crt -days 365 -sha256
   rm -f ${PKI_DIR}/${cert}.csr
+done
+
+openssl genrsa -out ${PKI_DIR}/front-proxy-client.key 2048
+openssl req -new -key ${PKI_DIR}/front-proxy-client.key -subj "/CN=front-proxy-client" -out ${PKI_DIR}/front-proxy-client.csr
+openssl x509 -req -in ${PKI_DIR}/front-proxy-client.csr \
+  -CA ${PKI_DIR}/front-proxy-ca.crt -CAkey ${PKI_DIR}/front-proxy-ca.key -CAcreateserial \
+  -out ${PKI_DIR}/front-proxy-client.crt -days 365 -sha256
+rm -f ${PKI_DIR}/front-proxy-client.csr
+
+ETCD_CERTS=(peer healthcheck-client)
+for cert in "${ETCD_CERTS[@]}"; do
+  openssl genrsa -out ${ETCD_DIR}/${cert}.key 2048
+  openssl req -new -key ${ETCD_DIR}/${cert}.key -subj "/CN=${cert}" -out ${ETCD_DIR}/${cert}.csr
+  openssl x509 -req -in ${ETCD_DIR}/${cert}.csr \
+    -CA ${ETCD_DIR}/ca.crt -CAkey ${ETCD_DIR}/ca.key -CAcreateserial \
+    -out ${ETCD_DIR}/${cert}.crt -days 365 -sha256
+  rm -f ${ETCD_DIR}/${cert}.csr
 done
 
 CRITICAL_CERTS=(
@@ -32,15 +64,17 @@ for cert in "${CRITICAL_CERTS[@]}"; do
   [ "$dir" != "." ] && mkdir -p "$PKI_DIR/$dir"
   openssl genrsa -out ${PKI_DIR}/${cert}.key 2048
   openssl req -new -key ${PKI_DIR}/${cert}.key -subj "/CN=${cert}" -out ${PKI_DIR}/${cert}.csr
-  openssl x509 -req -in ${PKI_DIR}/${cert}.csr \
-    -CA ${PKI_DIR}/ca.crt -CAkey ${PKI_DIR}/ca.key -CAcreateserial \
-    -out ${PKI_DIR}/${cert}.crt -days 1 -sha256
+  if [[ "$cert" == etcd/* ]]; then
+    openssl x509 -req -in ${PKI_DIR}/${cert}.csr \
+      -CA ${ETCD_DIR}/ca.crt -CAkey ${ETCD_DIR}/ca.key -CAcreateserial \
+      -out ${PKI_DIR}/${cert}.crt -days 1 -sha256
+  else
+    openssl x509 -req -in ${PKI_DIR}/${cert}.csr \
+      -CA ${PKI_DIR}/ca.crt -CAkey ${PKI_DIR}/ca.key -CAcreateserial \
+      -out ${PKI_DIR}/${cert}.crt -days 1 -sha256
+  fi
   rm -f ${PKI_DIR}/${cert}.csr
 done
 
 chown -R root:root ${PKI_DIR}
-chmod -R 600 ${PKI_DIR}/*.key ${ETCD_DIR}/*.key 2>/dev/null || true
-
-for c in $(find ${PKI_DIR} -name "*.crt"); do
-  printf " - %-40s %s\n" "$(basename $c)" "$(openssl x509 -in $c -noout -enddate)"
-done
+chmod 600 $(find ${PKI_DIR} -type f -name "*.key") || true
