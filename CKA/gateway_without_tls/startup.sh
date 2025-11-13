117f071040f7c3e175356118547d5f7a3dc3e1c2
agmtzg <5817993+AGMTZG@users.noreply.github.com>
2025-11-13 11:18:22 -0600

    Updated gateway no tls scenario

--- CKA/gateway_without_tls/startup.sh
+++ CKA/gateway_without_tls/startup.sh
@@ -4,6 +4,8 @@ echo -e "\033[91m[INFO] Installing MetalLB...\033[0m"
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.9/config/manifests/metallb-native.yaml
 echo -e "\033[91m[INFO] Waiting for MetalLB pods to become ready...\033[0m"
 kubectl wait --namespace metallb-system --for=condition=ready pod --all --timeout=120s || true
+echo -e "\033[91m[INFO]  Please wait 10 seconds... \033[0m"
+sleep 10
 IP=$(ip -4 addr show enp1s0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | head -n1)
 PREFIX=$(echo $IP | cut -d'.' -f1-3)
 RANGE_START="${PREFIX}.240"
